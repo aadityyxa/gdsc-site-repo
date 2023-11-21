5096a53b4cc1caeee7d387404c7279f32eccab3f
aadityaa <aditya.oza256@gmail.com>
2023-11-21 21:51:43 +0530

    added footer

---
+++ src/common-components/footer/footer.jsx
@@ -0,0 +1,30 @@
+import gdscBWLogo from '../../assets/gdsc-logo-bw.svg'
+import './footer.css'
+
+
+export default function Footer() {
+
+    return(
+        <div className='footer'>
+            <div className="logo">
+                <img src={gdscBWLogo} />
+            </div>
+
+            <div className="footer-sections">
+                <div className="footer-section">
+                    <span>EMAIL</span>
+                    <div>gdsc@gmail.com</div>
+                </div>
+
+                <div className="footer-section">
+                    <span>SOCIAL</span>
+                    <ul>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                        <li>LinkedIn</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
