74fb805134cf49d3a5b5578eacc51a8622437186
aadityaa <aditya.oza256@gmail.com>
2023-11-26 21:18:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './index.css'
 // import Homepage from './pages/homepage/homepage'
+import Error from './pages/error/error'
+import Events from './pages/events/events'
 import Podcast from './pages/podcast/podcast'
+import Homepage from './pages/homepage/homepage';
 
 function App() {
 
   return (
-    <Podcast />
+    <Error />
   )
 }
 
